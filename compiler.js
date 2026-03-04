e624e5de394bb9a2a1eec632d691fcabd33be7e0
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2026-03-03 19:31:51 -0500

    Test AST structure

--- compiler.js
+++ compiler.js
@@ -21,6 +21,30 @@ try {
 	console.error("Error reading file:", err);
 }
 
+class Op { // Operator generalizes class, function, statement, and expression definitions because I'm crazy
+
+	constructor(type = "NOP", args = []) {
+		this.type = type;
+		this.args = args;
+	}
+
+	print(prefix = "") {
+
+		if (this.args.length != 0) {
+
+			console.log(prefix + "[" + this.type + "]:");
+
+			for (let arg of this.args)
+				arg.print(prefix + "  ");
+
+		} else {
+			console.log(prefix + this.type);
+		}
+	}
+}
+
+new Op("=", [new Op("x"), new Op("+", [new Op("5"), new Op("10")])]).print();
+
 function sanitize(string) {
 
 	// remove comments
